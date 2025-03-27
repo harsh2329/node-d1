9b8b6d09782b6876249486295bc3e1e221e6d1a9
harsh soni <hsoni2392003@gmail.com>
2025-03-28 01:01:50 +0530

    offer api with file update

--- src/routes/OfferRoutes.js
+++ src/routes/OfferRoutes.js
@@ -6,5 +6,7 @@ routes.get('/alloffer', OfferController.getAllOffers);
 routes.get('alloffer/:id', OfferController.getOfferById);
 routes.put('alloffer/:id', OfferController.updateOffer);
 routes.delete('alloffer/:id', OfferController.deleteOffer);
+// routes.post('addOfferWithFile' , OfferController.addOfferWithFile);
+routes.post('/addOfferWithFile', OfferController.addOfferWithFile);
 
 module.exports = routes;
